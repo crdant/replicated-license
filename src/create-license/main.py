63747631dc1f151f345de295b79745fcbabb41d7
Chuck D'Antonio <chuck@crdant.io>
2024-02-25 11:37:06 -0500

    Adds license and download outputs (#2)
    TL;DR
    -----
    
    Provides the license and embedded cluster download info
    
    Details
    -------
    
    Updates the create license functionality to store the license file
    in S3 and provide outputs for the license file URI, embedded cluster
    download URI, and the token required to download the EC tarball.
    These changes set the license creation functionality up to be
    incorporated into a broader CloudFormation template that installs
    the application after creating the customer.

--- src/create-license/main.py
+++ src/create-license/main.py
@@ -4,9 +4,11 @@ from crhelper import CfnResource
 import logging
 
 import boto3
-aws_client = boto3.client('secretsmanager')
+secrets_manager = boto3.client('secretsmanager')
+s3 = boto3.client('s3')
 
 from customer import Customer
+from app import App
 
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -22,7 +24,7 @@ helper = CfnResource(
 
 def get_api_token():
   secret_arn = os.environ["SECRET_ARN"]
-  response = aws_client.get_secret_value(SecretId=secret_arn)
+  response = secrets_manager.get_secret_value(SecretId=secret_arn)
   return response['SecretString']
 
 def handler(event, context):
@@ -31,13 +33,33 @@ def handler(event, context):
 @helper.create
 def create(event, context):
     logger.info("creating resoource")
+
     api_token = get_api_token()
     properties = event.get('ResourceProperties')
+    logger.debug("Loading application")
+    app = App(api_token, properties.get('AppId'))
+
     expiration_date = datetime.date.today() + relativedelta(years=1)
     logger.debug("creating customer")
     customer = Customer.create(api_token, properties.get('Name'), properties.get('Email'),
-                               properties.get('AppId'), expiration_date, properties.get('Type'),
+                               app.id, expiration_date, properties.get('Type'),
                                properties.get('Channel'))  
+
+    license_id = customer.installationId
+    helper.Data.update({'DownloadToken': f'{license_id}'})
+
+    license_data = customer.license()
+    bucket_name = os.environ["LICENSE_BUCKET_NAME"]
+    bucket_domain = os.environ["LICENSE_BUCKET_DOMAIN"]
+    file_key = "{customer}.yaml".format(customer=customer.id)
+    logger.debug(f'saving license to file {file_key} in bucket {bucket_name}')
+    s3.put_object(Bucket=bucket_name, Key=file_key, Body=license_data)
+    helper.Data.update({'LicenseFileUri': f'https://{bucket_domain}/{file_key}'})
+
+    app_domain = app.api_host
+    slug = app.slug
+    channel = properties.get('Channel').lower()
+    helper.Data.update({'InstallerUri': f'https://{app_domain}/embedded/{slug}/{channel}'})
 
     return customer.id
 
@@ -49,5 +71,10 @@ def delete(event, context):
     properties = event.get('ResourceProperties')
 
     customer = Customer(api_token, properties.get('AppId'), customerId)
+
+    bucket_name = os.environ["LICENSE_BUCKET_NAME"]
+    file_key = "{customer}.yaml".format(customer=customer.id)
+    s3.delete_object(Bucket=bucket_name, Key=file_key)
+
     customer.remove()
 
