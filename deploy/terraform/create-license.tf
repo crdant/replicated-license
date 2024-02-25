ae61270f8b8bf8735cdacd52345bdcb94adf1967
Chuck D'Antonio <chuck@crdant.io>
2024-02-25 11:31:29 -0500

    Adds license and download outputs
    TL;DR
    -----
    
    Provides the license and embedded cluster download info
    
    Details
    -------
    
    Updates the create license functionality to store the license file
    in S3 and provide outputs for the license file URI, embedded cluster
    download URI, and the token required to download the EC tarball.
    These changes set the license creation functionality up to be
    incorporated into a broader CloudFormation template that installs
    the application after creating the customer.

--- deploy/terraform/create-license.tf
+++ deploy/terraform/create-license.tf
@@ -8,6 +8,31 @@ resource "aws_secretsmanager_secret_version" "api_token" {
   secret_string = var.api_token
 }
 
+resource "aws_s3_bucket" "licenses" {
+  bucket = "slackernews-license-${random_pet.bucket_suffix.id}"
+
+}
+
+resource "aws_s3_bucket_ownership_controls" "licenses" {
+  bucket = aws_s3_bucket.licenses.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "licenses" {
+  depends_on = [ aws_s3_bucket_ownership_controls.licenses ]
+
+  bucket = aws_s3_bucket.licenses.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "licenses" {
+  bucket = aws_s3_bucket.licenses.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_lambda_function" "create_license" {
   function_name = "create-replicated-license"
   architectures = ["arm64"]
@@ -19,20 +44,42 @@ resource "aws_lambda_function" "create_license" {
   filename         = "${var.build_directory}/create-license.zip"
   source_code_hash = filebase64sha256("${var.build_directory}/create-license.zip")
   
+  timeout = 6
+
   environment {
     variables = {
       SECRET_ARN = aws_secretsmanager_secret.api_token.arn
+      LICENSE_BUCKET_NAME = aws_s3_bucket.licenses.bucket
+      LICENSE_BUCKET_DOMAIN = aws_s3_bucket.licenses.bucket_regional_domain_name
     }
   }
 }
 
-resource "aws_iam_role_policy" "lambda_secretsmanager_policy" {
-  name   = "lambda_secretsmanager_policy"
-  role   = aws_iam_role.lambda_exec_role.id
-  policy = data.aws_iam_policy_document.lambda_secretsmanager_policy.json
+resource "aws_iam_policy" "lambda_license_bucket" {
+  name   = "lambda_license_bucket"
+  policy = data.aws_iam_policy_document.lambda_license_bucket.json
 }
 
-data "aws_iam_policy_document" "lambda_secretsmanager_policy" {
+data "aws_iam_policy_document" "lambda_license_bucket" {
+  statement {
+    actions   = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:PutObject",
+    ]
+    resources = [ "${aws_s3_bucket.licenses.arn}/*" ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_secrets_manager" {
+  name   = "lambda_secrets_manager"
+  policy = data.aws_iam_policy_document.lambda_secrets_manager.json
+}
+
+data "aws_iam_policy_document" "lambda_secrets_manager" {
   statement {
     actions   = ["secretsmanager:GetSecretValue"]
     resources = [aws_secretsmanager_secret.api_token.arn]
@@ -41,6 +88,16 @@ data "aws_iam_policy_document" "lambda_secretsmanager_policy" {
 
 data "aws_iam_policy" "lambda_exec_policy" {
   name = "AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_license_bucket" {
+  role       = aws_iam_role.lambda_exec_role.id
+  policy_arn = aws_iam_policy.lambda_license_bucket.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_secrets_manager" {
+  role       = aws_iam_role.lambda_exec_role.id
+  policy_arn = aws_iam_policy.lambda_secrets_manager.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
@@ -64,5 +121,4 @@ resource "aws_iam_role" "lambda_exec_role" {
     ],
   })
 }
-
 
